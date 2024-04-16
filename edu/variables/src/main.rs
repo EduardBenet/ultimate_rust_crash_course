c4c691988c2b3f8cc5e6a21ef3a6805283f0ddc2
Ubuntu <EduardBenet@RustTest.34qbfnapxyzuvmf2bp1ehsujpe.ax.internal.cloudapp.net>
2024-04-16 22:02:00 +0000

    var project

--- edu/variables/src/main.rs
+++ edu/variables/src/main.rs
@@ -1,6 +1,6 @@
 const STARTING_MISSILES:i32 = 8;
 const READY_AMOUNT: i32 = 2;
-fn main() {let (ready, mut missiles):(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
+fn main() {let (ready, missiles):(i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
     println!("{} missiles left", missiles-ready);
 }
